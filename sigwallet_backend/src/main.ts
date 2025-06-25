431f247bfa89f04b0fa3169651c45b593df8a292
Izie Zahba <dushara.fahzad62@gmail.com>
2025-06-25 16:41:31 +0800

    test: connection between backend and frontend

--- sigwallet_backend/src/main.ts
+++ sigwallet_backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO: Set Hardhat
+// TODO: Set Kysely
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('apitest/');
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error starting server:', err);
+});
