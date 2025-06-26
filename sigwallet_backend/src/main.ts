b9dd91eeea35ff0bdfba8ed9751e803de8c5ac09
Izie Zahba <dushara.fahzad62@gmail.com>
2025-06-26 14:22:33 +0800

    feat:add hardhat to sigwallet_contract folder

--- sigwallet_backend/src/main.ts
+++ sigwallet_backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// TODO: Set Hardhat
 // TODO: Set Kysely
 
 async function bootstrap() {
---
+++ sigwallet_contract/hardhat.config.ts
@@ -0,0 +1,14 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.28",
+    networks: {
+        localhost: {
+            chainId: 31337,
+            url: "http://127.0.0.1:8545",
+        },
+    },
+};
+
+export default config;
