2d9ecb09336b7a2056c5823b11de81a62ee132bf
Izie Zahba <dushara.fahzad62@gmail.com>
2025-06-26 16:14:28 +0800

    feat: create database connection and migration file

---
+++ sigwallet_backend/src/database/migrations/1750925454833_create_users_table.ts
@@ -0,0 +1,22 @@
+import { Kysely, sql } from 'kysely';
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('users')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('email', 'varchar(255)', (col) => col.notNull().unique())
+    .addColumn('name', 'varchar(255)', (col) => col.notNull())
+    .addColumn('created_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn('updated_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .execute();
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('users').execute();
+}
---
+++ sigwallet_frontend/lib/services/api_service.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+// TODO: Add a good way to communicate with the API
+
+Future<void> pingBackend() async {
+  var client = http.Client();
+
+  try {
+    http.Response response = await client.get(
+      Uri.parse('http://10.0.2.2:3000/'),
+    );
+
+    debugPrint(response.body);
+  } catch (e) {
+    debugPrint("Failed to ping: $e");
+    debugPrint("Failure");
+  }
+}
